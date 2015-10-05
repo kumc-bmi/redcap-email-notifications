507a406e609c7030d9852d8bd35d57e7c57ad8db
Michael Prittie <mprittie@kumc.edu>
2015-10-05 08:25:23 -0500

    Included use of config file, cleaned up variable names and documentation.

--- hooks.php
+++ hooks.php
@@ -3,29 +3,39 @@
  * This is a prototype implementation of the redcap_save_record hook which sends 
  * email notifications ...
  */
+define('PLUGIN_ROOT', realpath(REDCAP_ROOT.'plugins/'));
+define('NOTIFICATIONS_CONFIG', dirname(__FILE__).'/config.ini');
+
+
 function notifications_save_record($project_id, $record, $instrument, $event_id,
                                    $group_id, $survey_hash, $response_id)
 {
     // Provides access to REDCap helper functions and database connection.
     global $conn; // REDCapism
-    require_once('../../redcap_connect.php');
+    require_once(REDCAP_ROOT.'redcap_connect.php');
     // Evaluates REDCap branching logic syntax.
     require_once(APP_PATH_DOCROOT.'Classes/LogicTester.php');
     // Provides properly formated REDCap record data for use with LogicTester.
     require_once(APP_PATH_DOCROOT.'Classes/Records.php');
-    // Procodes Event based helper functions.
+    // Provides Event based helper functions.
     require_once(APP_PATH_DOCROOT.'Classes/Event.php');
 
     // This differs from REDCap's Record in that I can query for records in a 
     // given project by a field other than record id.
     require_once('records.php');
 
-    // This needs to be move to some sort of config.
-    define('NOTIFICATION_PID', 155);
+    // Load configuration...
+    // TODO: PluginConfig needs to be localized or generalized.
+    require_once(PLUGIN_ROOT.'/repower/utils/PluginConfig.php');
+    $CONFIG = new PluginConfig(dirname(__FILE__).'/config.ini');
     
     // Does the given project have associated notifications.
-    $notifications = get_records_by('project_id', $project_id, NOTIFICATION_PID,
-                                     $conn);
+    $notifications = get_records_by(
+        'project_id',
+        $project_id,
+        $CONFIG['notifications_pid'],
+        $conn
+    );
 
     // Iterate over notifications associated with the given project.
     foreach($notifications as $notification) {
@@ -41,28 +51,31 @@ function notifications_save_record($project_id, $record, $instrument, $event_id,
         }
 
         // Get and format submitted record data.
-        $saved_data = Records::getData('array', $record);
-        $prepared_data = $saved_data[$record][$event_id];
-        $fields = array_keys($saved_data[$record][$event_id]);
+        $raw_data = Records::getData('array', $record);
+        $event_data = $raw_data[$record]; // Should be record
+        $record_data = $event_data[$event]; // Should be event ... or field_data
+
+//        $saved_data = Records::getData('array', $record);
+//        $prepared_data = $saved_data[$record][$event_id];
+        $fields = array_keys($record_data);
         $fields = array_map(
             parameterize,
             $fields
         );
-        $values = array_values($saved_data[$record][$event_id]);
+        $values = array_values($record_data);
 
         // Does the given record meet notification logic conditions.
         if(LogicTester::isValid($logic)) {
-            if(LogicTester::apply($logic, $saved_data[$record])) {
+            if(LogicTester::apply($logic, $event_data)) {
                 if($notification['send_notification_field']) {
-                    if($prepared_data[$notification['send_notification_field']]) {
+                    if($record_data[$notification['send_notification_field']]) {
                         $send_notification_reset = array(array(
                             'record' => $record,
-                            //'field_name' => $notification['send_notification_field'],
-                            'field_name' => 'send_notification',
+                            'field_name' => $notification['send_notification_field'],
                             'value' => 0
                         ));
                         list($success, $error_msg) = save_redcap_data(
-                            'http://bmidev1.kumc.edu/redcap/api/',
+                            $CONFIG['api_url'],
                             $notification['project_token'],
                             $send_notification_reset
                         );
@@ -99,7 +112,8 @@ function notifications_save_record($project_id, $record, $instrument, $event_id,
             // Log that notification logic is invalid.
             error_log(
                 'Invalid notification logic in notification defined by '
-                .'(pid:'.NOTIFICATION_PID.'; rid:'.$notification['record_id'].') '
+                .'(pid:'.$CONFIG['notifications_pid'].'; '
+                .'rid:'.$notification['record_id'].') '
                 .'originating from action on '
                 .'(pid:'.$project_id.'; rid:'.$record.')'
             );
--- records.php
+++ records.php
@@ -103,8 +103,7 @@ function get_records_by($field_name, $value, $pid, $conn) {
 }
 
 function save_redcap_data($redcap_url, $api_token, $data) {
-//    require_once('./utils/RestCallRequest.php');
-    require_once('/srv/www/htdocs-insecure/redcap/plugins/notifications/utils/RestCallRequest.php');
+    require_once(dirname(__FILE__).'/utils/RestCallRequest.php');
 
     $api_request = new RestCallRequest(
         $redcap_url,

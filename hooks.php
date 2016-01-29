81babf51c20b30db4ea7b011f1c6b7c098780312
Michael Prittie <mprittie@kumc.edu>
2016-01-29 11:38:41 -0600

    Added functionality for calendar invites.

--- hooks.php
+++ hooks.php
@@ -4,6 +4,10 @@
  * preconfigured email notifications when an associated REDCap record is save,
  * and trigger conditions are meet.
  */
+define('RC_YES', 1);
+define('RC_NO', 0);
+define('RC_FORM_COMPLETE', 2);
+
 function notifications_save_record($project_id, $record, $instrument, $event_id,
                                    $group_id, $survey_hash, $response_id)
 {
@@ -12,7 +16,8 @@ function notifications_save_record($project_id, $record, $instrument, $event_id,
     require_once(REDCAP_ROOT.'redcap_connect.php');
 
     // Load configuration plugin configuration.
-    require_once(dirname(__FILE__).'/utils/PluginConfig.php');
+    define('FRAMEWORK_ROOT', REDCAP_ROOT.'plugins/framework/');
+    require_once(FRAMEWORK_ROOT.'/PluginConfig.php');
     $CONFIG = new PluginConfig(dirname(__FILE__).'/notifications.ini');
 
     // This differs from REDCap's Record class in that project records can be
@@ -242,11 +247,121 @@ function send_notification($notification, $record, $event_id, $record_data) {
         $record_data
     );
 
-    if(REDCap::email($to, $from, $subject, $body)) {
-        return true;
+    if($notification['calendar_event'] == RC_YES) {
+        $rdata = $record_data[$record][$event_id];
+        if($notification['event_all_day'] == RC_YES) {
+            $start_datetime = new DateTime(
+                $rdata[$notification['event_start_date']]
+            );
+            $end_datetime = clone $start_datetime;
+            $end_datetime->modify("+ 1 day");
+        } elseif($notification['event_set_length'] == RC_YES) {
+            $start_datetime = new DateTime(
+                $rdata[$notification['event_start_datetime']]
+            );
+            $end_datetime = clone $start_datetime;
+            $end_datetime->modify('+ '.$notification['event_length'].' minute');
+        } else {
+            $start_datetime = new DateTime(
+                $rdata[$notification['event_start_datetime']]
+            );
+            $end_datetime = new DateTime(
+                $rdata[$notification['event_end_datetime']]
+            );
+        }
+
+        send_ical_event(
+            $from,
+            $to,
+            $start_datetime->format('m/d/Y H:i:s'),
+            $end_datetime->format('m/d/Y H:i:s'),
+            $subject,
+            $body,
+            $notification['event_location']
+        );
     } else {
-        error_log('Failed to send email notification ...');
-        return false;
+
+        if(REDCap::email($to, $from, $subject, $body)) {
+            return true;
+        } else {
+            error_log('Failed to send email notification ...');
+            return false;
+        }
     }
+}
+
+function send_ical_event($from, $to, $start, $end, $subject, $body, $location) {
+   $domain = 'kumc.edu';
+
+    //Create Email Headers
+    $mime_boundary = "----Meeting Booking----".MD5(TIME());
+
+    $headers = "From: <".$from.">\n";
+    $headers .= "Reply-To: <".$from.">\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-Type: multipart/alternative; boundary=\"$mime_boundary\"\n";
+    $headers .= "Content-class: urn:content-classes:calendarmessage\n";
+
+    //Create Email Body (HTML)
+    $message = "--$mime_boundary\r\n";
+    $message .= "Content-Type: text/html; charset=UTF-8\n";
+    $message .= "Content-Transfer-Encoding: 8bit\n\n";
+    $message .= "<html>\n";
+    $message .= "<body>\n";
+    $message .= $body;
+    $message .= "</body>\n";
+    $message .= "</html>\n";
+    $message .= "--$mime_boundary\r\n";
+
+    $ical = 'BEGIN:VCALENDAR' . "\r\n" .
+    'PRODID:-//Microsoft Corporation//Outlook 10.0 MIMEDIR//EN' . "\r\n" .
+    'VERSION:2.0' . "\r\n" .
+    'METHOD:REQUEST' . "\r\n" .
+    'BEGIN:VTIMEZONE' . "\r\n" .
+    'TZID:America/Chicago' . "\r\n" .
+    'X-LIC-LOCATION:America/Chicago' . "\r\n" .
+    'BEGIN:DAYLIGHT' . "\r\n" .
+    'TZOFFSETFROM:-0600' . "\r\n" .
+    'TZOFFSETTO:-0500' . "\r\n" .
+    'TZNAME:CDT' . "\r\n" .
+    'DTSTART:19700308T020000' . "\r\n" .
+    'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU' . "\r\n" .
+    'END:DAYLIGHT' . "\r\n" .
+    'BEGIN:STANDARD' . "\r\n" .
+    'TZOFFSETFROM:-0500' . "\r\n" .
+    'TZOFFSETTO:-0600' . "\r\n" .
+    'TZNAME:CST' . "\r\n" .
+    'DTSTART:19701101T020000' . "\r\n" .
+    'RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU' . "\r\n" .
+    'END:STANDARD' . "\r\n" .
+    'END:VTIMEZONE' . "\r\n" .
+    'BEGIN:VEVENT' . "\r\n" .
+    'ORGANIZER;CN="":MAILTO:'.$from. "\r\n" .
+    'ATTENDEE;ROLE=REQ-PARTICIPANT;RSVP=TRUE:MAILTO:'.$to. "\r\n" .
+    'LAST-MODIFIED:' . date("Ymd\TGis") . "\r\n" .
+    'UID:'.date("Ymd\TGis", strtotime($start)).rand()."@".$domain."\r\n" .
+    'DTSTAMP:'.date("Ymd\TGis"). "\r\n" .
+    'DTSTART;TZID="America/Chicago":'.date("Ymd\THis", strtotime($start)). "\r\n" .
+    'DTEND;TZID="America/Chicago":'.date("Ymd\THis", strtotime($end)). "\r\n" .
+    'TRANSP:OPAQUE'. "\r\n" .
+    'SEQUENCE:1'. "\r\n" .
+    'SUMMARY:' . $subject . "\r\n" .
+    'LOCATION:' . $location . "\r\n" .
+    'CLASS:PUBLIC'. "\r\n" .
+    'PRIORITY:5'. "\r\n" .
+    'BEGIN:VALARM' . "\r\n" .
+    'TRIGGER:-PT15M' . "\r\n" .
+    'ACTION:DISPLAY' . "\r\n" .
+    'DESCRIPTION:Reminder' . "\r\n" .
+    'END:VALARM' . "\r\n" .
+    'END:VEVENT'. "\r\n" .
+    'END:VCALENDAR'. "\r\n";
+    $message .= 'Content-Type: text/calendar;name="meeting.ics";method=REQUEST'."\n";
+    $message .= "Content-Transfer-Encoding: 8bit\n\n";
+    $message .= $ical;
+
+    $mailsent = mail($to, $subject, $message, $headers);
+
+    return ($mailsent)?(true):(false); 
 }
 ?>
